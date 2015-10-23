aa3e992fafce9eaf5ddec4ae00b6b6a5983a09cb
BoyanP <bgpeychoff@gmail.com>
2015-10-23 16:00:09 -0600

    added OOSLogin.php, needs to be finished

---
+++ OOSLogin.php
@@ -0,0 +1,47 @@
+<html>
+
+<head>
+
+	<title>
+
+		Welcome
+
+	</title>
+
+
+</head>
+
+
+
+<body>
+
+	what's up 
+
+	<?php
+
+	$username;
+	$password;
+	$typeOfUser;
+
+	if (isset ($_POST['validate'])){
+	
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+
+	}
+
+
+
+	?>
+
+	
+
+
+
+</body>
+
+
+
+
+
+</html>
