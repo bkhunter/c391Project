a139b1d8b845d7274e8c9dcef0fbc58647eaa3d0
Ben Hunter <bkhunter@ualberta.ca>
2015-10-28 16:07:23 -0600

    have a bare bones button screen for the sensor and user Module

---
+++ sensorAdd.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+
+	<title>
+
+		Sensor or Module Page
+
+	</title>
+
+
+</head>
+
+
+
+<body>
+
+	Temporary! Need to add functionality!!!!
+
+</body>
---
+++ sensorModule.php
@@ -0,0 +1,69 @@
+<html>
+	<head>
+
+	<title>
+
+		Sensor and User Management
+	</title>
+
+
+	</head>
+
+	<link rel="stylesheet" type="text/css" href= "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel ="stylesheet" type ="text/css" href="sensorModule.css">
+	
+	<body>
+
+		<div class ="page">
+		<div class = "page-header">
+		<h1 class ="title"> Sensor and User Management</h1>			
+		</div>
+		
+
+		<div class="LoginForm container">
+
+		<form name = "create sensors" method="post"  action="sensorAdd.php"> 
+					<h2 class ="LoginHeader"> Manage Sensors </h2>
+					<input type="submit" value="Create Sensors">
+	
+		</form>
+		</div>
+		
+		<div class="LoginForm container">
+
+		<form name = "remove sensors" method="post"  action="sensorAdd.php"> 
+					<input type="submit" value="Remove Sensors">
+	
+		</form>
+		</div>
+		
+		<div class="LoginForm container">
+
+		<form name = "add account" method="post"  action="sensorAdd.php"> 
+					<h2 class ="loginHeader"> Manage User Accounts </h2>
+					<input type="submit" value="Add User Account">
+	
+		</form>
+		</div>
+		
+		<div class="LoginForm container">
+
+		<form name = "update account" method="post"  action="sensorAdd.php"> 
+					<input type="submit" value="Update User Account">
+	
+		</form>
+		</div>
+		
+		<div class="LoginForm container">
+
+		<form name = "remove account" method="post"  action="sensorAdd.php"> 
+					<input type="submit" value="Remove User Account">
+	
+		</form>
+		</div>
+		
+		</div>
+	</body>
+
+
+</html>
