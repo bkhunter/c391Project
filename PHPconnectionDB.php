3562a9864f852286ebc10263cb0894f332dcb151
aylmao <aylmao@users.noreply.github.com>
2015-10-28 15:14:36 -0600

    connect

---
+++ PHPconnectionDB.php
@@ -0,0 +1,11 @@
+<?php
+function connect(){
+	$conn = oci_connect('test', 'test');
+	if (!$conn) {
+		$e = oci_error();
+		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	}
+
+	return $conn;
+}
+?>
