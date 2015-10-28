061d79a4e230692c32e36635dff1165bc5e9b2c4
aylmao <aylmao@users.noreply.github.com>
2015-10-28 15:17:44 -0600

    gitgnore

--- PHPconnectionDB.php
+++ PHPconnectionDB.php
@@ -1,6 +1,6 @@
 <?php
 function connect(){
-	$conn = oci_connect('test', 'test');
+	$conn = oci_connect('volivare', 'playmagic125');
 	if (!$conn) {
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
