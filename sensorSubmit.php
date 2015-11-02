72e1bcd41c7a1d311973d75050f72518e2b091b6
Ben Hunter <bkhunter@ualberta.ca>
2015-11-02 12:54:53 -0700

    more work on sensor stuff

--- sensorSubmit.php
+++ sensorSubmit.php
@@ -1,19 +1,48 @@
+<?php
+include("PHPconnectionDB.php");
+?>
+
 <html>
     <body>
-	<h1>Sensor Created</h1>
+		<h1>Sensor Created</h1>
 	
-	<?php   // first method    	 
-		if(isset($_POST['create'])){        	
-			$loc=$_POST['location'];            		
-			$sType=$_POST['sensor_type'];
-			$desc=$_POST['description'];
-	           	echo'Your Location is '.$loc.'<br/> 
-	           	Your sensor type is '.$sType.'<br/>
-	           	Your description says: '.$desc.'';       
-		}	
-	?>
+	
+		<TABLE BORDER=1>
+		<?php   // first method    	 
+			if(isset($_POST['create'])){        	
+				$loc=$_POST['location'];            		
+				$sType=$_POST['sensor_type'];
+				$desc=$_POST['description'];
+				
+				ini_set('display_errors', 1);
+	    		error_reporting(E_ALL);
+	    	
+	    		$conn=connect();
+	    		
+	    		if (!$conn) {
+    				$e = oci_error();
+    				trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	    		} else {
+	    			echo 'connected' ;	
+	    		}
+			} ?>
+				
+				<TR>
+					<TD>Location</TD>
+					<TD> <?php echo $loc ?></TD>
+				</TR>
+				<TR>
+					<TD>Sensor Type</TD>
+					<TD><?php echo $sType ?></TD>
+				</TR>
+				<TR>
+					<TD>Description</TD>
+					<TD><?php echo $desc ?></TD>
+				</TR>
+	        
+		</TABLE>
 
-	<br/>
+		<br/>
 	
     </body>
     
