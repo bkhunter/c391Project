96ef93c82c4d6af0d127dfa2f00d4a5b798c1fee
aylmao <aylmao@users.noreply.github.com>
2015-11-05 13:48:09 -0700

    info now how logout.php logs user out

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?php
 		
 		session_start();
+		/*
+		$_SESSION['validate'] != 'true' is checked in OOS.php 
+		if it's true than an account is valid 
+		but if it is not true echo the msg and destroy session thus logging out the user. 
+		*/
 		$_SESSION['validate'] = '<center><font color="#DF88FD">Please log in</font></center>';
 		header('Location: OOS.php', true, 301);
 		exit();
