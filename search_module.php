0a2942b3dee5e9466d742fe26c4b532da2afbbe9
BoyanP <bgpeychoff@gmail.com>
2015-11-11 18:50:40 -0700

    subscribe almost done, gotta fix query for unsubscribing

---
+++ search_module.php
@@ -0,0 +1,66 @@
+<?php
+include("PHPconnectionDB.php");
+
+session_start();
+
+?>
+
+
+<html>
+
+<head>
+
+<title>OOS - Search</title>
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="searchModule.css">
+
+</head>
+
+
+
+
+<body>
+
+
+
+	<div class = 'page-header'>
+
+			<h1>Ocean Observation System 
+				<small>Search your Sensors</small></h1>
+
+	</div>
+
+
+	<div class = 'container'>
+
+
+
+
+	<div>
+
+
+		<form></form>
+
+
+
+
+	</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+</body>
+
+
+
+</html>
