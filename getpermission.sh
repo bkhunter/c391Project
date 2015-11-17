3fece729f7cbf7dca82c28dc757e8b2649da2fff
aylmao <aylmao@users.noreply.github.com>
2015-11-16 17:39:27 -0700

    get permission.sh

---
+++ getpermission.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+chmod 644 *.php
+chmod 644 *.css
+cd sensor_and_user_mgmt
+chmod 644 *.php
+chmod 644 *.css
+cd ..
+cd dc
+chmod 644 *.php
+chmod 644 *.css
+cd ..
+cd Data_Analysis
+chmod 644 *.php
+chmod 644 *.css
+cd ..
+
