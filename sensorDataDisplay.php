96e971c67140530255e7f737acc3d644b1db6db3
BoyanP <bgpeychoff@gmail.com>
2015-11-21 02:38:49 -0700

    sensorDataDisplay has queries. must test and display properly

--- sensorDataDisplay.php
+++ sensorDataDisplay.php
@@ -36,25 +36,82 @@ session_start();
 			$data['keywordSearch'] = $_POST['keywordSearch'];
 			$data['keywords'] = preg_split("/(\s)|,/g", $data['keywordSearch']);
 
+
 		} 
 		
 		if($_POST['locationSearch']!=''){
 				
 			$data['location'] = $_POST['locationSearch'];
+
+
 			
 
 		}
 
-		if($_POST[''])
+		if($_POST['FromSearch']!=''){
+
+			$data['FromSearch']=$_POST['FromSearch'];
+			$data['UntilSearch']=$_POST['UntilSearch'];
+
+	
+			
+
+		}
 
 
 		if(isset($_POST['dataTypeA'])){
 
+			$sqlA = 'select * from audio_recordings, subscriptions, sensors	
+							where subscriptions.person_id = \''.$_SESSION['person_id'].'\'
+							and subscriptions.sensor_id =sensors.sensor_id
+							and audio_recordings.sensor_id = sensor_id
+							';
+		
+			if($data['location']!=''){
 
-				
+					$sqlA = $sqlA .'and sensor_id.location = \''.$data['location'].'\'';
+
+			}
+
+			if(sizeof($data['keywords'])>0){
+
+				//check all keywords
+				foreach($data['keywords'] as  $keyword){
+
+					$sqlA = $sqlA .'and audio_recordings.description LIKE \''.$keyword.'\'';		
+
+				}
+
+			}
+
+			if($data['FromSearch']!=''){
+					
+					
+					//$sqlA = $sqlA .'and audio_recordings.date';
+
+					$formattedFromDate = explode('/', $data['FromSearch']);
+
+					$month = $formatttedFromDate[0];
+					$day = $formattedFromDate[1];
+					$year = $formattedFromDate[2];
+
+					$sqlFromDate = $day.$month.$year;
+					
+					$formattedFromDate = explode('/', $data['UntilSearch']);
+					
+					$month = $formatttedFromDate[0];
+					$day = $formattedFromDate[1];
+					$year = $formattedFromDate[2];
+					
+
+					$sqlUntilDate = $day.$month.$year;
 
 
-		}
+					$sqlA = $sqlA . 'and audio_recordings.date_created between \''.$sqlFromDate.'\' and \''.$sqlUntilDate.'\'';
+			}
+
+
+		}//if audioRecordings
 
 		if(isset($_POST['dataTypeI'])){
 
