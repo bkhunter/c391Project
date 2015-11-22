d0ecabac3a6d4dee4ef5916e64f9c70004d0cf9e
BoyanP <bgpeychoff@gmail.com>
2015-11-21 23:02:38 -0700

    search half working

--- sensorDataDisplay.php
+++ sensorDataDisplay.php
@@ -2,177 +2,7 @@
 include("PHPconnectionDB.php");
 
 session_start();
-
-function getAudioData(){
-
-	$sqlA = 'select * from audio_recordings, subscriptions, sensors	
-		where subscriptions.person_id = \''.$_SESSION['person_id'].'\'
-		and subscriptions.sensor_id =sensors.sensor_id
-		and audio_recordings.sensor_id = sensor_id';
-		
-	if($data['location']!=''){
-
-		$sqlA = $sqlA .'and sensor_id.location = \''.$data['location'].'\'';
-
-	}
-
-
-	if(sizeof($data['keywords'])>0){
-
-		//check all keywords
-		foreach($data['keywords'] as  $keyword){
-			$sqlA = $sqlA .'and audio_recordings.description LIKE \''.$keyword.'\'';		
-
-		}
-
-	}
-
-	if($data['FromSearch']!=''){
-					
-		//$sqlA = $sqlA .'and audio_recordings.date';
-		$formattedFromDate = explode('/', $data['FromSearch']);
-
-
-		$month = $formatttedFromDate[0];
-		$day = $formattedFromDate[1];
-		$year = $formattedFromDate[2];
-		
-		$sqlFromDate = $day.$month.$year;
-		$formattedFromDate = explode('/', $data['UntilSearch']);
-					
-		$month = $formatttedFromDate[0];
-		$day = $formattedFromDate[1];
-		$year = $formattedFromDate[2];
-					
-
-		$sqlUntilDate = $day.$month.$year;
-
-		$sqlA = $sqlA . 'and audio_recordings.date_created between \''.$sqlFromDate.'\' and \''.$sqlUntilDate.'\'';
-	}
-
-	$parsedAudioData = oci_parse($conn,$sqlA);
-	$res = oci_execute($parsedAudioData);
-			
-
-
-
-
-
-
-}
-
-function getScalarData(){
-	
-		$sqlS = 'select * from images, subscriptions, sensors	
-							where subscriptions.person_id = \''.$_SESSION['person_id'].'\'
-							and subscriptions.sensor_id =sensors.sensor_id
-							and audio_recordings.sensor_id = sensor_id
-							';
-		
-			if($data['location']!=''){
-
-					$sqlS = $sqlS .'and sensor_id.location = \''.$data['location'].'\'';
-
-			}
-
-			if($data['FromSearch']!=''){
-					
-					
-					//$sqlA = $sqlA .'and audio_recordings.date';
-
-					$formattedFromDate = explode('/', $data['FromSearch']);
-
-					$month = $formatttedFromDate[0];
-					$day = $formattedFromDate[1];
-					$year = $formattedFromDate[2];
-
-					$sqlFromDate = $day.$month.$year;
-					
-					$formattedFromDate = explode('/', $data['UntilSearch']);
-					
-					$month = $formatttedFromDate[0];
-					$day = $formattedFromDate[1];
-					$year = $formattedFromDate[2];
-					
-
-					$sqlUntilDate = $day.$month.$year;
-
-
-					$sqlS = $sqlS . 'and audio_recordings.date_created between \''.$sqlFromDate.'\' and \''.$sqlUntilDate.'\'';
-			}
-
-
-			$parsedScalarData = oci_parse($conn,$sqlS);
-			$res = oci_execute($parsedScalarData);
-
-
-
-
-
-}
-
-function getImageData(){
-
-
-
-	$sqlI = 'select * from images, subscriptions, sensors	
-							where subscriptions.person_id = \''.$_SESSION['person_id'].'\'
-							and subscriptions.sensor_id =sensors.sensor_id
-							and audio_recordings.sensor_id = sensor_id
-							';
-		
-	if($data['location']!=''){
-
-		$sqlI = $sqlI .'and sensor_id.location = \''.$data['location'].'\'';
-
-	}
-
-	if(sizeof($data['keywords'])>0){
-
-		//check all keywords
-		foreach($data['keywords'] as  $keyword){
-
-			$sqlI = $sqlI .'and audio_recordings.description LIKE \''.$keyword.'\'';		
-
-		}
-
-	}
-
-	if($data['FromSearch']!=''){
-					
-					
-					//$sqlA = $sqlA .'and audio_recordings.date';
-
-					$formattedFromDate = explode('/', $data['FromSearch']);
-
-					$month = $formatttedFromDate[0];
-					$day = $formattedFromDate[1];
-					$year = $formattedFromDate[2];
-
-					$sqlFromDate = $day.$month.$year;
-					
-					$formattedFromDate = explode('/', $data['UntilSearch']);
-					
-					$month = $formatttedFromDate[0];
-					$day = $formattedFromDate[1];
-					$year = $formattedFromDate[2];
-					
-
-					$sqlUntilDate = $day.$month.$year;
-
-
-					$sqlI = $sqlI . 'and audio_recordings.date_created between \''.$sqlFromDate.'\' and \''.$sqlUntilDate.'\'';
-	}
-
-	$parsedImageData = oci_parse($conn,$sqlI);
-	$res = oci_execute($parsedImageData);
-
-
-}
-
 ?>
-
-
 <html>
 	<head>
 
@@ -182,6 +12,7 @@ function getImageData(){
 
 		</title>
 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 	</head>
 
@@ -189,21 +20,19 @@ function getImageData(){
 	<body>
 
 	<div>
-		<h1>Ocean Observation System
+	
+	<div class='container'>
+	<h1>Ocean Observation System
     	<small> Search Sensor Data</small>
     </h1>
-
 	<?php
 
 
 		$data;
-		$conn = connect();
-
-		if($_POST['keywordSearch']!=''){
-
-			$data['keywordSearch'] = $_POST['keywordSearch'];
-			$data['keywords'] = preg_split("/(\s)|,/g", $data['keywordSearch']);
-
+		//$conn = connect();
+		if($_POST['keyWordSearch']!=''){
+			
+			$data['keywords'] = preg_split('/(\s)+|,/m', $_POST['keyWordSearch']);
 
 		} 
 		
@@ -217,10 +46,8 @@ function getImageData(){
 		}
 
 		if($_POST['FromSearch']!=''){
-
 			$data['FromSearch']=$_POST['FromSearch'];
 			$data['UntilSearch']=$_POST['UntilSearch'];
-
 	
 			
 
@@ -228,20 +55,19 @@ function getImageData(){
 
 
 		if(isset($_POST['dataTypeA'])){
-
-			getAudioData();
+			getAudioData($data);
 
 			
 		}//if audioRecordings
 
 		if(isset($_POST['dataTypeI'])){
-			getImageData();
+			getImageData($data);
 
 		}//end if images
 
 		if(isset($_POST['dataTypeS'])){
 
-			getScalarData();
+			getScalarData($data);
 		
 
 		}//end if scalardata
@@ -250,22 +76,247 @@ function getImageData(){
 
 		if(!isset($_POST['dataTypeA']) && !isset($_POST['dataTypeI']) && !isset($_POST['dataTypeS'])){
 
-			getAudioData();
-			getImageData();
-			getScalarData();
+			getAudioData($data);
+			getImageData($data);
+			getScalarData($data);
 			
 
 		}
 
-	//oci_fetch($parsedAudioData)
-	//oci_fetch($parsedScalarData)
-	//oci_fetch($parsedImageData)
+
+function getAudioData($data){
+
+	$sqlA = 'select audio_recordings.recording_id ,audio_recordings.sensor_id ,audio_recordings.date_created ,audio_recordings.length, audio_recordings.description, 			audio_recordings.recorded_data , sensors.location
+		from audio_recordings, subscriptions, sensors	
+		where subscriptions.person_id = 1
+		and subscriptions.sensor_id = sensors.sensor_id
+		and audio_recordings.sensor_id = subscriptions.sensor_id ';
 		
+	if($data['location']!=''){
+
+		$sqlA = $sqlA .'and sensors.location = \''.$data['location'].'\' ';
+
+	}
+
+
+	if(sizeof($data['keywords'])>0){
+		//check all keywords
+		foreach($data['keywords'] as  $keyword){
+			$sqlA = $sqlA .'and audio_recordings.description LIKE \'%'.$keyword.'%\' ';		
+
+		}
+
+	}
+
+	if($data['FromSearch']!=''){
+
+		$formattedFromDate = explode('/', $data['FromSearch']);
+
+		$month = $formattedFromDate[0];
+		$day = $formattedFromDate[1];
+		$year = $formattedFromDate[2];
+		
+		$sqlFromDate = $day.'/'.$month.'/'.$year;
+		$formattedUntilDate = explode('/', $data['UntilSearch']);
+					
+		$month = $formatttedUntilDate[0];
+		$day = $formattedUntilDate[1];
+		$year = $formattedUntilDate[2];
+					
+
+		$sqlUntilDate = $day.'/'.$month.'/'.$year;
+		echo 'from: '.$sqlFromDate.'<br>';
+		echo 'until: '.$sqlUntilDate.'<br>';
+		$sqlA = $sqlA . 'and audio_recordings.date_created between TO_DATE(\''.$sqlFromDate.'\' ,\'dd/mm/yyyy\')  and  TO_DATE( \''.$sqlUntilDate.'\',\'dd/mm/yyyy\') ';
+	}
+	echo $sqlA;
+	$conn = connect();
+	$parsedAudioData = oci_parse($conn,$sqlA);
+	$res = oci_execute($parsedAudioData);
+	
+	echo '<h3>Audio Recordings</h3>';	
+	echo'<table> <thead> <tr> 
+	<th> ID </th> <th> SensorID</th> <th>length</th> <th>date</th> <th> Description</th> <th>Download</th> </tr>
+	</thead><tbody>';
+
+
+	while(oci_fetch($parsedAudioData)){
+		$audioData['audioID'] = oci_result($parsedAudioData,'RECORDING_ID');
+		$audioData['sensorID'] = oci_result($parsedAudioData, 'SENSOR_ID');
+		$audioData['length'] = oci_result($parsedAudioData,'LENGTH');
+		$audioData['date'] = oci_result($parsedAudioData , 'DATE_CREATED');
+		$audioData['description'] = oci_result($parsedAudioData , 'DESCRIPTION');
+
+		echo '<tr><td>'.$audioData['audioID'].'</td><td>'.$audioData['sensorID'].'</td>
+		<td>'.$audioData['length'].'</td> <td>'.$audioData['date'].'</td> <td>'.$audioData['description'].'</td>
+		<td> <button id = '.$audioData['audioID'].'>Download</button></td></tr>';
+
+
+	}	
+
+	echo '</table>';
+
+
+
+
+}
+
+function getScalarData($data){
+	
+		$sqlS = 'select scalar_data.id, scalar_data.sensor_id , scalar_data.date_created, scalar_data.value
+		from scalar_data, subscriptions, sensors	
+		where subscriptions.person_id = \''.$_SESSION['person_id'].'\'
+		and subscriptions.sensor_id =sensors.sensor_id
+		and scalar_data.sensor_id = sensors.sensor_id ';
+		
+			if($data['location']!=''){
+
+					$sqlS = $sqlS .'and sensors.location = \''.$data['location'].'\' ';
+
+			}
+
+			if($data['FromSearch']!=''){
+
+					$formattedFromDate = explode('/', $data['FromSearch']);
+
+					$month = $formattedFromDate[0];
+					$day = $formattedFromDate[1];
+					$year = $formattedFromDate[2];
+
+					$sqlFromDate = $day.'/'.$month.'/'.$year;
+					
+					$formattedUntilDate = explode('/', $data['UntilSearch']);
+					
+					$month = $formattedUntilDate[0];
+					$day = $formattedUntilDate[1];
+					$year = $formattedUntilDate[2];
+					
+
+					$sqlUntilDate = $day.'/'.$month.'/'.$year;
+
+
+					$sqlS = $sqlS . 'and scalar_data.date_created between TO_DATE(\''.$sqlFromDate.'\' ,\'dd/mm/yyyy\')  and  TO_DATE( \''.$sqlUntilDate.'\',\'dd/mm/yyyy\') ';
+			}
+
+			$conn = connect();
+			$parsedScalarData = oci_parse($conn,$sqlS);
+			$res = oci_execute($parsedScalarData);
+			echo $sqlS;
+			echo '<h3>Scalar Data</h3>';
+			echo'<table> <thead> <tr> 
+			<th> ID </th> <th> SensorID</th> <th>value</th> <th>date</th><th>Download</th> </tr>
+			</thead><tbody>';
+
+			while(oci_fetch($parsedScalarData)){
+				$scalarData['ID'] = oci_result($parsedScalarData,'ID');
+				$scalarData['sensorID'] = oci_result($parsedScalarData, 'SENSOR_ID');
+				$scalarData['value'] = oci_result($parsedScalarData, 'VALUE');
+				$scalarData['date'] = oci_result($parsedScalarData,'DATE_CREATED');
+				
+				echo '<tr><td>'.$scalarData['ID'].'</td><td>'.$scalarData['sensorID'].'</td> 
+				<td>'.$scalarData['value'].'</td> <td>'.$scalarData['date'].'</td>
+				<td> <button id = '.$scalarData['ID'].'>Download</button></td></tr>';
+
+
+			}	
+
+	echo '</table>';
+
+
+
+
+
+}
+
+function getImageData($data){
+
+
+
+	$sqlI = 'select images.image_id , images.sensor_id, images.date_created, images.description , images.thumbnail , images.recoreded_data  
+	from images, subscriptions, sensors	
+	where subscriptions.person_id = \''.$_SESSION['person_id'].'\'
+	and subscriptions.sensor_id =sensors.sensor_id
+	and images.sensor_id = sensors.sensor_id';
+		
+	if($data['location']!=''){
+
+		$sqlI = $sqlI .'and sensor_id.location = \''.$data['location'].'\' ';
+
+	}
+
+	if(sizeof($data['keywords'])>0){
+
+		//check all keywords
+		foreach($data['keywords'] as  $keyword){
+
+			$sqlI = $sqlI .'and images.description LIKE \'%'.$keyword.'%\' ';		
+
+		}
+
+	}
+
+	if($data['FromSearch']!=''){
+					
+
+					$formattedFromDate = explode('/', $data['FromSearch']);
+
+					$month = $formatttedFromDate[0];
+					$day = $formattedFromDate[1];
+					$year = $formattedFromDate[2];
+
+					$sqlFromDate = $day.'/'.$formattedFromDate[0].'/'.$year;
+					
+					$formattedUntilDate = explode('/', $data['UntilSearch']);
+					
+					$month = $formatttedUntilDate[0];
+					$day = $formattedUntilDate[1];
+					$year = $formattedUntilDate[2];
+					
+
+					$sqlUntilDate = $day.'/'.$formattedUntilDate[0].'/'.$year;
+
+
+					$sqlI = $sqlI . 'and images.date_created between TO_DATE(\''.$sqlFromDate.'\' ,"dd/mm/yyyy")  and  TO_DATE( \''.$sqlUntilDate.'\',"dd/mm/yyyy") ';
+	}
+
+	echo $sqlI;
+	$conn = connect();
+	$parsedImageData = oci_parse($conn,$sqlI);
+	$res = oci_execute($parsedImageData);
+
+	echo '<h3> Image Data</h3>';
+
+	echo'<table> <thead> <tr> 
+	<th> ID </th> <th> SensorID</th> <th>Thumbnail</th>  <th>Date</th> <th>Description</th> <th>Download</th> </tr>
+	</thead><tbody>';
+
+	
+	while(oci_fetch($parsedImageData)){
+
+		$imageData['ID'] = oci_result($parsedImageData,'IMAGE_ID');
+		$imageData['sensorID'] = oci_result($parsedImageData, 'SENSOR_ID');
+		$imageData['thumbnail'] = oci_result($parsedImageData,'THUMBNAIL');
+		$imageData['length'] = oci_result($parsedImageData,'LENGTH');
+		$imageData['description'] = oci_result($parsedImageData, 'DESCRIPTION');
+				
+		echo '<tr><td>'.$imageData['ID'].'</td><td>'.$imageData['sensorID'].'</td> <td> <img src="data:image/jpeg;base64, '.$imageData['thumbnail']->load().'">
+		<td>'.$imageData['length'].'</td> <td>'.$imageData['value'].'</td> <td>'.$imageData['date'].'</td>
+		<td> <button id = '.$imageData['ID'].'>Download</button></td></tr>';
+
+
+		
+
+
+	}
+
+
+}
+
 
 	?>
 
-	
-	<div>
+	</div>
+	</div>
 
 	</body>
 
@@ -274,3 +325,16 @@ function getImageData(){
 
 
 </html>
+
+<?php
+
+
+?>
+
+
+
+
+
+
+
+
