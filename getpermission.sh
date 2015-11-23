b518f79f1be1a934d0057a775dc5f0a6c0237a9e
aylmao <aylmao@users.noreply.github.com>
2015-11-23 13:31:36 -0700

    Update getpermission.sh

--- getpermission.sh
+++ getpermission.sh
@@ -2,17 +2,21 @@
 
 chmod 644 *.php
 chmod 644 *.css
+chmod -R 777 sensor_and_user_mgmt
 cd sensor_and_user_mgmt
 chmod 644 *.php
 chmod 644 *.css
 cd ..
+chmod -R 777 dc
 cd dc
 chmod 644 *.php
 chmod 644 *.css
 cd ..
+chmod -R 777 Data_Analysis
 cd Data_Analysis
 chmod 644 *.php
 chmod 644 *.css
+chmod -R 777 Daily
 cd Daily
 cd ..
 cd ..
