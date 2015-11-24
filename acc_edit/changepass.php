85a3f86af19926d6cd53af0e9f9687fdb97d8dad
aylmao <aylmao@users.noreply.github.com>
2015-11-23 20:36:50 -0700

    resolved: comment was being displayed on page

--- acc_edit/changepass.php
+++ acc_edit/changepass.php
@@ -35,14 +35,14 @@ if($_SESSION['login'] != 'true') {
 	<center><h1>Ocean Observation System</h1></center>
 	
 	
-	//help button 
+	<!--//help button -->
 	<div align="right">
 			<form name = "login" method="post"  action="../help.html"> 
 					<input type="submit" name="validate" value="help" style="width: 125px; height: 50px;">
 			</form>
 	</div> 
 	
-	//password form 
+	<!--//password form -->
 	<form name = "editInfo" method="post"  action="accountupdate.php"> 
 					<center><h4 class ="editHeader"> personal account settings you can change: </h4></center>
 					<center>Change password: <input type = "password" name = "newpass">
